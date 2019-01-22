85dc6a4ffcf416385530564aa503463b6495912f
kirbycampbell <jkirbycampbell@gmail.com>
2019-01-22 14:39:32 -0800

    Todo Hooks replacing Redux

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
-import React from "react";
+import React, { useContext, useReducer } from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import TodosContext from "./context";
+import todosReducer from "./reducer";
+import TodoList from "./components/TodoList";
 
-export const UserContext = React.createContext();
+const App = () => {
+  const initialState = useContext(TodosContext);
+  const [state, dispatch] = useReducer(todosReducer, initialState);
 
-const username = "Dave";
+  return (
+    <TodosContext.Provider value={{ state, dispatch }}>
+      <TodoList />
+    </TodosContext.Provider>
+  );
+};
 
-ReactDOM.render(
-  <UserContext.Provider value={username}>
-    <App username={username} />{" "}
-  </UserContext.Provider>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
