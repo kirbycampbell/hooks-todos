30e84a11cff02f99e8e5a8c44103cc18e2053e7b
kirbycampbell <jkirbycampbell@gmail.com>
2019-01-22 22:41:10 -0800

    Added useAPI

--- src/context.js
+++ src/context.js
@@ -1,11 +1,7 @@
 import React from "react";
 
 const TodosContext = React.createContext({
-  todos: [
-    { id: 1, text: "Eat breakfast", complete: true },
-    { id: 2, text: "Eat Lunch", complete: false },
-    { id: 3, text: "Eat Dinner", complete: false }
-  ]
+  todos: []
 });
 
 export default TodosContext;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,39 @@
-import React, { useContext, useReducer } from "react";
+import React, { useContext, useReducer, useEffect, useState } from "react";
 import ReactDOM from "react-dom";
+import axios from "axios";
 import * as serviceWorker from "./serviceWorker";
 import TodosContext from "./context";
 import todosReducer from "./reducer";
 import TodoList from "./components/TodoList";
 import TodoForm from "./components/TodoForm";
 
+const useAPI = endpoint => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = async () => {
+    const response = await axios.get(endpoint);
+    setData(response.data);
+  };
+  return data;
+};
 const App = () => {
   const initialState = useContext(TodosContext);
   const [state, dispatch] = useReducer(todosReducer, initialState);
+  const savedTodos = useAPI("https://hooks-api-ezxn5u70x.now.sh/todos");
 
+  useEffect(
+    () => {
+      dispatch({
+        type: "GET_TODOS",
+        payload: savedTodos
+      });
+    },
+    [savedTodos]
+  );
   return (
     <TodosContext.Provider value={{ state, dispatch }}>
       <TodoForm />
